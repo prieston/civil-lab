0fad48986e7cd3658dc57da616a452a6f5f56d23
Theofilos Papadopoulos <theofilos.p@kissmybutton.gr>
2021-11-24 14:08:39 +0200

    first commit

---
+++ clip/clip.js
@@ -0,0 +1,51 @@
+import { HTMLClip, CSSEffect } from "@donkeyclip/motorcortex";
+import initParams from "./initParams";
+
+export const clip = new HTMLClip({
+  html: `
+    <div class="container">
+        <img src="./assets/rest.png">
+        <img src="./assets/civil.png">
+    </div>`,
+  css: `
+  .container {
+    width:100%;
+    height:100%;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items: center;
+    color: {{ initParams.color }};
+  }
+  `,
+  host: document.getElementById("clip"),
+  containerParams: {
+    width: "800px",
+    height: "450px",
+  },
+  initParamsValidationRules: {
+    color: {
+      label: "Text Color",
+      type: "color",
+      optional: true,
+      default: "white",
+    },
+  },
+  initParams: initParams[1].value,
+});
+
+const MyIncident = new CSSEffect(
+  {
+    animatedAttrs: {
+      transform: {
+        scale: 2,
+      },
+    },
+  },
+  {
+    selector: ".container",
+    duration: 2000,
+    easing: "linear",
+  }
+);
+clip.addIncident(MyIncident, 0);
---
+++ clip/id.js
@@ -0,0 +1,1 @@
+export default "fcc854af-611b-42d7-8402-16a0012aed51";
